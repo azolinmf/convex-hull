7133c5b4420692f25fe8334d470ba64cb64074cd
Maria Fernanda Azolin <azolin.mf@gmail.com>
2021-07-02 22:28:40 -0300

    adds distance between points method

--- hello.cpp
+++ hello.cpp
@@ -5,17 +5,18 @@
 
 using namespace std;
 
-class Point {       
-  public:             
-    int x;       
-    int y;  
+class Point
+{
+public:
+    int x;
+    int y;
 };
 
 vector<Point> readCoordinates()
 {
     ifstream inFile;
     inFile.open("my_file.txt");
-    
+
     if (inFile.fail())
     {
         cerr << "Error opening a file" << endl;
@@ -24,7 +25,7 @@ vector<Point> readCoordinates()
     }
     string line;
     int x, y;
-    int num_coordinates; 
+    int num_coordinates;
 
     inFile >> num_coordinates;
     vector<Point> coordinates;
@@ -48,8 +49,14 @@ vector<Point> readCoordinates()
     return coordinates;
 }
 
-float getDistanceBetweenPoints(Point point) {
-    return sqrt(pow(point.x, 2) + pow(point.y, 2));
+float getDistanceBetweenPoints(Point p1, Point p2)
+{
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+}
+
+float calculateVectorCrossProduct() 
+{
+
 }
 
 int main()
@@ -57,20 +64,12 @@ int main()
     vector<Point> coordinates;
 
     coordinates = readCoordinates();
-    
-    for (int i=0; i <= coordinates.size() - 1; i++) {
+
+    for (int i = 0; i <= coordinates.size() - 1; i++)
+    {
         cout << coordinates[i].x << " " << coordinates[i].y << endl;
     }
-
-    Point a;
-    a.x = 2;
-    a.y = 3;
-
-    Point b;
-    b.x = 5;
-    b.y = 6;
-
-    cout << getDistanceBetweenPoints(a, b) << endl;
+    
 
     return 0;
 }
