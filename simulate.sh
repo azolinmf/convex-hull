894a5273976117c314eeb957f1d34082a91d3676
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2021-07-19 23:46:24 -0300

    - Updated simulate script

--- simulate.sh
+++ simulate.sh
@@ -13,3 +13,10 @@ make
 mv hull simulate
 cd simulate
 ./hull input.txt
+cp ../scripts/fecho.plot .
+cp ../scripts/pontos.plot .
+npts=$(head -1 input.txt); tail -$npts input.txt > input2.txt
+gnuplot ../scripts/pontos.plot
+cp fecho.txt fecho2.txt; head -1 fecho.txt >> fecho2.txt
+gnuplot fecho.plot
+open pontos.pdf fecho.pdf 
